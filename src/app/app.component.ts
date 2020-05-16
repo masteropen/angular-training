8b05bf8fb1cf35d0164eaf9465762618ae861f47
majid noureddine <noureddine-majid@hotmail.fr>
2020-05-16 02:26:57 +0100

    finalize routing chapter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import { AppareilService } from './services/appareil.service';
 
 @Component({
   selector: 'app-root',
@@ -8,38 +7,9 @@ import { AppareilService } from './services/appareil.service';
 })
 export class AppComponent implements OnInit {
 
-  isAuth = false;
-  lastUpdate = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-        () => {
-          resolve(date);
-        }, 2000
-    );
-  });
-  appareils: any[];
-
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-        () => {
-          this.isAuth = true;
-        }, 4000
-    );
-  }
-
-  ngOnInit() {
-    this.appareils = this.appareilService.appareils;
-  }
-
-  onAllumer() {
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre() {
-    if (confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
-      this.appareilService.switchOffAll();
-    } else {
-      return null;
+    constructor() {
     }
-  }
+
+    ngOnInit() {
+    }
 }
