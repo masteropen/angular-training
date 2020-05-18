626f645c1e15d9d4b6570314173d8406a348a3ed
majid noureddine <noureddine-majid@hotmail.fr>
2020-05-18 01:52:28 +0100

    reactive form validation

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {UserService} from '../services/user.service';
 import {Router} from '@angular/router';
 import {User} from '../models/User.model';
@@ -24,10 +24,11 @@ export class NewUserComponent implements OnInit {
 
   initForm() {
     this.userForm = this.formBuilder.group({
-      firstName: '',
-      lastName: '',
-      email: '',
-      drinkPreference: ''
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      drinkPreference: ['', Validators.required],
+      hobbies: this.formBuilder.array([])
     });
   }
 
@@ -37,9 +38,23 @@ export class NewUserComponent implements OnInit {
         formValue['firstName'],
         formValue['lastName'],
         formValue['email'],
-        formValue['drinkPreference']
+        formValue['drinkPreference'],
+        formValue['hobbies'] ? formValue['hobbies'] : []
     );
     this.userService.addUser(newUser);
     this.router.navigate(['/users']);
+  }
+
+  getHobbies(): FormArray {
+    return this.userForm.get('hobbies') as FormArray;
+  }
+
+  onAddHobby() {
+    const newHobbyControl = this.formBuilder.control(null, Validators.required);
+    this.getHobbies().push(newHobbyControl);
+  }
+
+  onRemoveHobby() {
+    this.getHobbies().removeAt(-1);
   }
 }
